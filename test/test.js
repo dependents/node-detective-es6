9a9e5489a3fde8521f4a4280ac850fefe956166d
noyobo <noyobo@gmail.com>
2020-11-26 11:03:57 +0800

    2.1.0

--- test/test.js
+++ test/test.js
@@ -1,103 +1,85 @@
-var assert = require('assert');
-var detective = require('../');
+var detective = require("../");
 
-describe('detective-es6', function() {
+describe("detective-es6", function () {
   var ast = {
-    type: 'Program',
-    body: [{
-      type: 'VariableDeclaration',
-      declarations: [{
-        type: 'VariableDeclarator',
-        id: {
-            type: 'Identifier',
-            name: 'x'
-        },
-        init: {
-            type: 'Literal',
-            value: 4,
-            raw: '4'
-        }
-      }],
-      kind: 'let'
-    }]
+    type: "Program",
+    body: [
+      {
+        type: "VariableDeclaration",
+        declarations: [
+          {
+            type: "VariableDeclarator",
+            id: {
+              type: "Identifier",
+              name: "x",
+            },
+            init: {
+              type: "Literal",
+              value: 4,
+              raw: "4",
+            },
+          },
+        ],
+        kind: "let",
+      },
+    ],
   };
 
-  it('accepts an ast', function() {
+  it("accepts an ast", function () {
     var deps = detective(ast);
-    assert(!deps.length);
+    expect(deps).toMatchSnapshot();
   });
 
-  it('retrieves the dependencies of es6 modules', function() {
+  it("retrieves the dependencies of es6 modules", function () {
     var deps = detective('import Abc, * as All from "mylib";');
-    assert(deps.length === 1);
-    assert(deps[0].name === 'mylib');
-    assert(deps[0].star === true);
-    assert(deps[0].alias === 'All');
-    assert(deps[0].default === 'Abc');
+    expect(deps).toMatchSnapshot();
   });
 
-  it('retrieves the re-export dependencies of es6 modules', function() {
+  it("retrieves the re-export dependencies of es6 modules", function () {
     var deps = detective('export {foo, bar} from "mylib";');
-    assert(deps.length === 1);
-    assert(deps[0].name === 'mylib');
-    assert(deps[0].members.length === 2);
-    assert(deps[0].members[0].name === 'foo');
-    assert(deps[0].members[0].alias === 'foo');
-    assert(deps[0].members[1].name === 'bar');
-    assert(deps[0].members[1].alias === 'bar');
+    expect(deps).toMatchSnapshot();
   });
 
-  it('retrieves the re-export dependencies alias of es6 modules', function() {
+  it("retrieves the re-export dependencies alias of es6 modules", function () {
     var deps = detective('export {foo as Foo} from "mylib";');
-    assert(deps.length === 1);
-    assert(deps[0].name === 'mylib');
-    assert(deps[0].members.length === 1);
-    assert(deps[0].members[0].name === 'foo');
-    assert(deps[0].members[0].alias === 'Foo');
+    expect(deps).toMatchSnapshot();
   });
 
-  it('retrieves the re-export * dependencies of es6 modules', function() {
+  it("retrieves the re-export * dependencies of es6 modules", function () {
     var deps = detective('export * from "mylib";');
-
-    assert(deps.length === 1);
-    assert(deps[0].name === 'mylib');
+    expect(deps).toMatchSnapshot();
   });
 
-  it('handles multiple imports', function() {
-    var deps = detective('import {foo as Foo, bar} from "mylib";\nimport "mylib2"');
+  it("handles multiple imports", function () {
+    var deps = detective(
+      'import {foo as Foo, bar} from "mylib";\nimport "mylib2"'
+    );
 
-    assert(deps.length === 2);
-    assert(deps[0].name === 'mylib');
-    assert(deps[0].members.length === 2);
-    assert(deps[0].members[0].name === 'foo');
-    assert(deps[0].members[0].alias === 'Foo');
-    assert(deps[0].members[1].name === 'bar');
-    assert(deps[0].members[1].alias === 'bar');
-    assert(deps[1].name === 'mylib2');
+    expect(deps).toMatchSnapshot();
   });
 
-  it('handles default imports', function() {
+  it("handles default imports", function () {
     var deps = detective('import foo from "foo";');
 
-    assert(deps.length === 1);
-    assert(deps[0].name === 'foo');
-    assert(deps[0].default === 'foo');
+    expect(deps).toMatchSnapshot();
   });
 
-  it('returns an empty list for non-es6 modules', function() {
+  it("returns an empty list for non-es6 modules", function () {
     var deps = detective('var foo = require("foo");');
-    assert(!deps.length);
+    expect(deps).toMatchSnapshot();
   });
 
-  it('does not throw with jsx in a module', function() {
-    assert.doesNotThrow(function() {
+  it("does not throw with jsx in a module", function () {
+    expect(() => {
       detective('import foo from "foo"; var templ = <jsx />;');
-    });
+    }).not.toThrow();
   });
 
-  it('does not throw on an async ES7 function', function() {
-    assert.doesNotThrow(function() {
-      detective('import foo from "foo"; export default async function foo() {}');
-    });
+  it("does not throw on an async ES7 function", function () {
+    expect(() => {
+      detective(
+        'import foo from "foo"; export default async function bar() {}'
+      );
+    }).not.toThrow();
   });
 });
